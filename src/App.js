75db6f4c729ce28cd1ec5c423cfb7486c35bc194
decoderstreak <uman.tab12@gmail.com>
2021-10-31 11:50:15 +0530

    Cards-done

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
+import Testimonials from './Components/Testimonials/Testimonials'
 
 function App() {
   return (
     <div className="App">
-      <h1>Hey coding_monks! let's rock the hackathon</h1>
+      {/* <h1>Hey coding_monks! let's rock the hackathon</h1> */}
+      <Testimonials />
+
     </div>
   );
 }
